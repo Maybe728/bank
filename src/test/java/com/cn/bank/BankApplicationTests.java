cc827ff5e15849e7b49510d6deb5fdc6ee4adfd8
Maybe728 <1007402145@qq.com>
2018-11-28 15:17:49 +0800

    实现分页功能

--- src/test/java/com/cn/bank/BankApplicationTests.java
+++ src/test/java/com/cn/bank/BankApplicationTests.java
@@ -1,7 +1,11 @@
 package com.cn.bank;
 
+import com.cn.bank.model.Bank;
+import com.cn.bank.service.BankService;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BankApplicationTests {
 
+    @Autowired
+    BankService bankService;
     @Test
     public void contextLoads() {
+        String bankId = "88888888";
+        Bank bank = new Bank();
+        bank.setBankId(bankId);
+        TestCase.assertNotNull(bankService.queryBankInfoByLatAndLng(bank));
     }
 
 }
